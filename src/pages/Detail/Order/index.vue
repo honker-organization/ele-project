0e36d3cd8e628706c0e82b2abb6ea6423ea87ac7
Huangyaoyi <1357182806@qq.com>
2020-09-18 23:50:41 +0800

    detail

---
+++ src/pages/Detail/Order/index.vue
@@ -0,0 +1,165 @@
+<template>
+  <div>
+    <div class="topAd">
+      <img
+        src="https://cube.elemecdn.com/a/0f/29ce988126f5e8146cf6d1d4f05f2png.png?x-oss-process=image/format,webp/resize,w_686"
+        alt=""
+      />
+    </div>
+    <div class="recommend">
+      <p class="title">商家推荐</p>
+      <van-swipe
+        class="my-swipe"
+        :autoplay="3000"
+        :width="292"
+        :loop="false"
+        indicator-color="white"
+      >
+        <van-swipe-item>
+          <div class="swipItem">
+            <img
+              src="https://cube.elemecdn.com/b/46/0aab566e756d1fa5bf2fb4ce6595cjpeg.jpeg?x-oss-process=image/resize,m_lfit,w_241/watermark,g_se,x_4,y_4,image_YS8xYS82OGRlYzVjYTE0YjU1ZjJlZmFhYmIxMjM4Y2ZkZXBuZy5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF8yOA%3D%3D/quality,q_90/format,webp"
+              alt=""
+            />
+            <div class="dishes">
+              <div class="cartTitle">招牌黄焖鸡+香菇+土豆+豆腐丝+米饭</div>
+              <p class="foodCart">月售16 好评率0%</p>
+              <div class="cartBottom">
+                <p class="price">￥19.9</p>
+                <van-icon name="add" />
+              </div>
+            </div>
+          </div>
+        </van-swipe-item>
+        <van-swipe-item>
+          <div class="swipItem">
+            <img
+              src="https://cube.elemecdn.com/b/46/0aab566e756d1fa5bf2fb4ce6595cjpeg.jpeg?x-oss-process=image/resize,m_lfit,w_241/watermark,g_se,x_4,y_4,image_YS8xYS82OGRlYzVjYTE0YjU1ZjJlZmFhYmIxMjM4Y2ZkZXBuZy5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF8yOA%3D%3D/quality,q_90/format,webp"
+              alt=""
+            />
+            <div class="dishes">
+              <div class="cartTitle">招牌黄焖鸡+香菇+土豆+豆腐丝+米饭</div>
+              <p class="foodCart">月售16 好评率0%</p>
+              <div class="cartBottom">
+                <p class="price">￥19.9</p>
+                <van-icon name="add" />
+              </div>
+            </div>
+          </div>
+        </van-swipe-item>
+        <van-swipe-item>
+          <div class="swipItem">
+            <img
+              src="https://cube.elemecdn.com/b/46/0aab566e756d1fa5bf2fb4ce6595cjpeg.jpeg?x-oss-process=image/resize,m_lfit,w_241/watermark,g_se,x_4,y_4,image_YS8xYS82OGRlYzVjYTE0YjU1ZjJlZmFhYmIxMjM4Y2ZkZXBuZy5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF8yOA%3D%3D/quality,q_90/format,webp"
+              alt=""
+            />
+            <div class="dishes">
+              <div class="cartTitle">招牌黄焖鸡+香菇+土豆+豆腐丝+米饭</div>
+              <p class="foodCart">月售16 好评率0%</p>
+              <div class="cartBottom">
+                <p class="price">￥19.9</p>
+                <van-icon name="add" />
+              </div>
+            </div>
+          </div>
+        </van-swipe-item>
+        <van-swipe-item>
+          <div class="swipItem">
+            <img
+              src="https://cube.elemecdn.com/b/46/0aab566e756d1fa5bf2fb4ce6595cjpeg.jpeg?x-oss-process=image/resize,m_lfit,w_241/watermark,g_se,x_4,y_4,image_YS8xYS82OGRlYzVjYTE0YjU1ZjJlZmFhYmIxMjM4Y2ZkZXBuZy5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF8yOA%3D%3D/quality,q_90/format,webp"
+              alt=""
+            />
+            <div class="dishes">
+              <div class="cartTitle">招牌黄焖鸡+香菇+土豆+豆腐丝+米饭</div>
+              <p class="foodCart">月售16 好评率0%</p>
+              <div class="cartBottom">
+                <p class="price">￥19.9</p>
+                <van-icon name="add" />
+              </div>
+            </div>
+          </div>
+        </van-swipe-item>
+        <van-swipe-item>
+          <div class="swipItem">
+            <img
+              src="https://cube.elemecdn.com/b/46/0aab566e756d1fa5bf2fb4ce6595cjpeg.jpeg?x-oss-process=image/resize,m_lfit,w_241/watermark,g_se,x_4,y_4,image_YS8xYS82OGRlYzVjYTE0YjU1ZjJlZmFhYmIxMjM4Y2ZkZXBuZy5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF8yOA%3D%3D/quality,q_90/format,webp"
+              alt=""
+            />
+            <div class="dishes">
+              <div class="cartTitle">招牌黄焖鸡+香菇+土豆+豆腐丝+米饭</div>
+              <p class="foodCart">月售16 好评率0%</p>
+              <div class="cartBottom">
+                <p class="price">￥19.9</p>
+                <van-icon name="add" />
+              </div>
+            </div>
+          </div>
+        </van-swipe-item>
+      </van-swipe>
+    </div>
+    <div class="orderPage">
+      <div class="navigation" id="resetNav">
+        <van-sidebar v-model="activeKey">
+          <van-sidebar-item class="asideItem" title="热销" />
+          <van-sidebar-item class="asideItem" title="优惠" />
+          <van-sidebar-item class="asideItem" title="必选品" />
+        </van-sidebar>
+      </div>
+      <div class="menu">
+        <van-swipe style="height: 200px;" vertical :loop="false">
+          <van-swipe-item class="menuItem">
+            <div class="image">
+              <img
+                src="https://cube.elemecdn.com/b/8b/5d5838426a19b575fdd4599f19f08jpeg.jpeg?x-oss-process=image/resize,m_lfit,w_141,h_141/watermark,g_se,x_4,y_4,image_YS8xYS82OGRlYzVjYTE0YjU1ZjJlZmFhYmIxMjM4Y2ZkZXBuZy5wbmc_eC1vc3MtcHJvY2Vzcz1pbWFnZS9yZXNpemUsUF8yOA%3D%3D/quality,q_90/format,webp"
+                alt=""
+              />
+            </div>
+            <div class="menuInfo">
+              <p class="infoTitle">招牌黄焖鸡+土豆+青菜+米饭</p>
+              <p class="material">
+                主要原料：鸡肉
+              </p>
+              <p class="sales"><span>月售67份</span><span>好评率100%</span></p>
+              <p class="activity">
+                <span>5.1折</span><span>每单限1份优惠</span
+                ><span class="limit">剩7份</span>
+              </p>
+              <div id="infoPrice">
+                <p>
+                  <span>￥19.9</span>
+                  <del>￥38.88</del>
+                </p>
+                <van-icon name="add" />
+              </div>
+            </div>
+          </van-swipe-item>
+        </van-swipe>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "./index.styl";
+import { Swipe, SwipeItem } from "vant";
+import { Icon } from "vant";
+import { Sidebar, SidebarItem } from "vant";
+export default {
+  name: "",
+  data() {
+    return {
+      activeKey: 0,
+    };
+  },
+  methods: {},
+  components: {
+    [Swipe.name]: Swipe,
+    [SwipeItem.name]: SwipeItem,
+    [Icon.name]: Icon,
+    [Sidebar.name]: Sidebar,
+    [SidebarItem.name]: SidebarItem,
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
---
+++ src/pages/Detail/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div>
+    <!-- 店铺头部 -->
+    <div class="header">
+      <div class="logo">
+        <div class="logoBg"></div>
+        <img
+          src="https://cube.elemecdn.com/5/84/9B3AB309F4BC594A8C36F80AC9F21jpeg.jpeg?x-oss-process=image/format,webp/resize,w_150"
+          alt=""
+          class="logoImg"
+        />
+      </div>
+      <div class="shop">
+        <div class="title">
+          <p>黄焖鸡(草围店)</p>
+          <van-icon class="arrow" name="play" />
+        </div>
+        <div class="sign">
+          <span>评价4.6</span>
+          <span>月售1110单</span>
+          <span>蜂鸟快送约40分钟</span>
+        </div>
+        <div class="coupon">
+          <div class="vipCoupon">
+            <p class="price">￥6</p>
+            <p>无门槛</p>
+            <p>领取</p>
+          </div>
+          <div class="ordinaryCoupon">
+            <p class="price">￥3</p>
+            <p>满50可用</p>
+            <p>领取</p>
+          </div>
+        </div>
+        <div class="reduction">
+          <div class="left">
+            <p><span>25减18</span></p>
+            <p><span>50减23</span></p>
+            <p><span>95减44</span></p>
+            <p><span>配送费优惠</span></p>
+          </div>
+
+          <div class="right">
+            <p>6个优惠</p>
+          </div>
+        </div>
+        <div class="notice">
+          <p>公告公告高嘎嘎嘎嘎嘎啊噶阿哥</p>
+        </div>
+      </div>
+    </div>
+    <!-- 店铺详情 -->
+    <div class="detaiContainer">
+      <van-tabs class="tabs" title-active-color="black" title-style="30px">
+        <van-tab class="title" title="点餐"></van-tab>
+        <van-tab class="title" title="评价"></van-tab>
+        <van-tab class="title" title="商家"></van-tab>
+      </van-tabs>
+    </div>
+    <!-- 点餐页 -->
+    <Order />
+    <!-- 评价页 -->
+    <!-- <Evaluate /> -->
+    <!-- 商家页 -->
+    <!-- <Business /> -->
+    <!-- 底部提交 -->
+    <div class="footer"></div>
+  </div>
+</template>
+
+<script>
+import { Icon } from "vant";
+import { Tab, Tabs } from "vant";
+import "./index.stylus";
+
+import Order from "./Order";
+import Evaluate from "./Evaluate";
+import Business from "./Business";
+export default {
+  name: "",
+  components: {
+    Order,
+    Evaluate,
+    Business,
+    [Icon.name]: Icon,
+    [Tab.name]: Tab,
+    [Tabs.name]: Tabs,
+  },
+};
+</script>
+
+<style lang="stylus" scoped></style>
